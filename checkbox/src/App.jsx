bbc69ae0ee291b2f2108d83f96e24ca213a4ca40
sherySSH <hussainsyedshaharyaar@gmail.com>
2024-10-13 20:22:38 +0500

    added main project to run the individual components for visualization; added checkbox input box

---
+++ checkbox/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+        <div className='input-box-checkbox'>
+            <p>Days:</p>
+            <div>
+                <label>1</label><input type='checkbox'></input>
+                <label>2</label><input type='checkbox'></input>
+                <label>3</label><input type='checkbox'></input>
+                <label>4</label><input type='checkbox'></input>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default App
